aa2e329a60522b6ba8d666adb788dddf0cef1165
Eugene Chou <euchou@ucsc.edu>
2024-02-13 01:03:03 -0800

    bump toolchain versions and build script

--- src/misc/toolchain-build.sh
+++ src/misc/toolchain-build.sh
@@ -1,51 +1,66 @@
 #!/bin/bash
 
 ######################################################
-# 
-# Setup OS lab tool chain (i386-elf-* cross compiler). 
+#
+# Setup OS lab tool chain (i386-elf-* cross compiler).
 #
 # Tested on Mac OS, Ubuntu and Fedora.
 #
-# Author: Ryan Huang <huang@cs.jhu.edu>
+# Authors: Ryan Huang <huang@cs.jhu.edu>
+#          Eugene Chou <euchou@ucsc.edu> (added version bump)
 #
 # Example Usage: ./toolchain-build.sh /home/ryan/318/toolchain
 #
 
-perror()
-{
+perror() {
   >&2 echo $1
   exit 1
 }
 
 download_and_check()
 {
-  local fname=$(basename $1)
-  local fdirname="${fname%.tar.*}"
+  local src=$(basename $1)
+  local sig=$(basename $2)
+  local dir="${src%.tar.*}"
+  local keyring=$3
+
   cd $CWD/src
-  if [ ! -f $fname ]; then
-    wget $1 
-    if [ ! -f $fname ]; then
-      perror "Failed to download $1"
+
+  # Download the source.
+  if [[ ! -f $src ]]; then
+    wget $1
+    if [[ ! -f $src ]]; then
+      perror "Failed to download $1 (source)"
     fi
   fi
-  local checksum=$(shasum -a 256 $fname | cut -d' ' -f 1)
-  if [ "$checksum" != "$2" ]; then
-    perror "Corrupted or missing source $1: expecting $2 got $checksum"
+
+  # Download the signature.
+  if [[ ! -f $sig ]]; then
+    wget $2
+    if [[ ! -f $sig ]]; then
+      perror "Failed to download $1 (signature)"
+    fi
   fi
-  echo "Downloaded and verified $fname from $1"
-  if [ ! -d $fdirname ]; then
-    echo "Extracting $fname to $fdirname..."
-    if [ $fname == *.tar.gz ]; then
-      tar xzf $fname 
-    elif [ $fname == *.tar.bz2 ]; then
-      tar xjf $fname
-    elif [ $fname == *.tar.xz ]; then
-      tar xJf $fname
+
+  # Verify the signature.
+  if ! gpgv --keyring $keyring $sig $src; then
+    perror "Failed to verify $1"
+  fi
+
+  echo "Downloaded and verified $src from $1"
+  if [ ! -d $dir ]; then
+    echo "Extracting $src to $fdirname..."
+    if [ $src == *.tar.gz ]; then
+      tar xzf $src
+    elif [ $src == *.tar.bz2 ]; then
+      tar xjf $src
+    elif [ $src == *.tar.xz ]; then
+      tar xJf $src
     else
-      perror "Unrecognized archive extension $fname"
+      perror "Unrecognized archive extension $src"
     fi
   else
-    echo "$fname is already extracted"
+    echo "$src is already extracted"
   fi
 }
 
@@ -132,6 +147,7 @@ else
     exit 1
   fi
 fi
+
 export PATH=$PREFIX/bin:$PATH
 if [ $os == "Linux" ]; then
   export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
@@ -143,28 +159,41 @@ fi
 
 TARGET=i386-elf
 
+# Latest as of 2024-02-13
+BINUTILS_VER="binutils-2.42"
+BINUTILS_SRC="https://ftp.gnu.org/gnu/binutils/$BINUTILS_VER.tar.gz"
+BINUTILS_SIG="https://ftp.gnu.org/gnu/binutils/$BINUTILS_VER.tar.gz.sig"
+
+# Latest as of 2024-02-13
+GCC_VER="gcc-13.2"
+GCC_SRC="https://ftp.gnu.org/gnu/binutils/$GCC_VER.tar.gz"
+GCC_SIG="https://ftp.gnu.org/gnu/binutils/$GCC_VER.tar.gz.sig"
+
+# Latest as of 2024-02-13
+GDB_VER="gdb-14.1"
+GDB_SRC="https://ftp.gnu.org/gnu/binutils/$GDB_VER.tar.gz"
+GDB_SIG="https://ftp.gnu.org/gnu/binutils/$GDB_VER.tar.gz.sig"
+
+# Download GPG keyring
+if [[ ! -f https://ftp.gnu.org/gnu/gnu-keyring.gpg ]]; then
+  wget https://ftp.gnu.org/gnu/gnu-keyring.gpg
+fi
+
 # Download sources
 if [ $tool == "all" -o $tool == "binutils" ]; then
-  download_and_check https://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz 26253bf0f360ceeba1d9ab6965c57c6a48a01a8343382130d1ed47c468a3094f
+  download_and_check $BINUTILS_SRC $BINUTILS_SIG $CWD/gnu-keyring.gpg
 fi
 if [ $tool == "all" -o $tool == "gcc" ]; then
-  download_and_check https://ftp.gnu.org/gnu/gcc/gcc-6.2.0/gcc-6.2.0.tar.bz2 9944589fc722d3e66308c0ce5257788ebd7872982a718aa2516123940671b7c5
-  if [ ! -d $CWD/src/gcc-6.2.0/mpfr ]; then
-    cd $CWD/src/gcc-6.2.0 && contrib/download_prerequisites || perror "Failed to download pre-requisite for GCC"
-  fi
-  echo "Patching GCC..."
-  pushd $CWD/src/gcc-6.2.0
-  cat $SCRIPT_DIR/gcc-6.2.0-ubsan.patch | patch -p2
-  popd
+  download_and_check $GCC_SRC $GCC_SIG $CWD/gnu-keyring.gpg
 fi
 if [ $tool == "all" -o $tool == "gdb" ]; then
-  download_and_check https://ftp.gnu.org/gnu/gdb/gdb-7.9.1.tar.xz cd9c543a411a05b2b647dd38936034b68c2b5d6f10e0d51dc168c166c973ba40
+  download_and_check $GDB_SRC $GDB_SIG $CWD/gnu-keyring.gpg
 fi
 
 if [ $tool == "all" -o $tool == "binutils" ]; then
   echo "Building binutils..."
   mkdir -p $CWD/build/binutils && cd $CWD/build/binutils
-  ../../src/binutils-2.27/configure --prefix=$PREFIX --target=$TARGET \
+  ../../src/$BINUTILS_VER/configure --prefix=$PREFIX --target=$TARGET \
     --disable-multilib --disable-nls --disable-werror || perror "Failed to configure binutils"
   make -j8 || perror "Failed to make binutils"
   make install
@@ -172,8 +201,8 @@ fi
 
 if [ $tool == "all" -o $tool == "gcc" ]; then
   echo "Building GCC..."
-  mkdir -p $CWD/build/gcc && cd $CWD/build/gcc 
-  ../../src/gcc-6.2.0/configure CXXFLAGS="-fpermissive" --prefix=$PREFIX --target=$TARGET \
+  mkdir -p $CWD/build/gcc && cd $CWD/build/gcc
+  ../../src/$GCC_VER/configure CXXFLAGS="-fpermissive" --prefix=$PREFIX --target=$TARGET \
     --disable-multilib --disable-nls --disable-werror --disable-libssp \
     --disable-libmudflap --with-newlib --without-headers --enable-languages=c,c++ || perror "Failed to configure gcc"
   make -j8 all-gcc  || perror "Failed to make gcc"
@@ -184,11 +213,14 @@ fi
 
 if [ $tool == "all" -o $tool == "gdb" ]; then
   echo "Building gdb..."
-  mkdir -p $CWD/build/gdb && cd $CWD/build/gdb 
-  ../../src/gdb-7.9.1/configure --prefix=$PREFIX --target=$TARGET --disable-werror || perror "Failed to configure gdb"
+  mkdir -p $CWD/build/gdb && cd $CWD/build/gdb
+  ../../src/$GDB_VER/configure --prefix=$PREFIX --target=$TARGET --disable-werror || perror "Failed to configure gdb"
   make -j8 || perror "Failed to make gdb"
   make install
 fi
+
+# Remove the keyring.
+rm -f gnu-keyring.gpg
 
 echo "************************************************************"
 echo "*                                                          *"
